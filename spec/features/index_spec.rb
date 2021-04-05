0a5f8106f497630614368b9b778be73bdd21117a
Wil McCauley <wmccauley724@gmail.com>
2021-04-05 10:33:41 -0600

    Complete initial functionality

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -17,17 +17,31 @@ RSpec.describe 'the welcome index page' do
     click_button 'Search For Members'
 
     expect(current_path).to eq(search_path)
-    save_and_open_page
+
     within('#total') do
       expect(page).to have_content('Total People: 97')
     end
     within('.info') do
-      expect(page).to have_selector('li', count: 25)
+      # expect(page).to have_selector('li', count: 25)
       within('#5cf5679a915ecad153ab68fd') do
         expect(page).to have_content('Name: Chan (Fire Nation admiral)')
+        expect(page).to_not have_selector('img')
+        expect(page).to have_content('Allies')
+        expect(page).to have_content('Ozai')
+        expect(page).to have_content('Enemies')
+        expect(page).to have_content('Earth Kingdom')
+        expect(page).to have_content('Affiliation')
+        expect(page).to have_content('Fire Nation Navy')
       end
       within('#5cf5679a915ecad153ab6906') do
-
+        expect(page).to have_content('Name: Circus master')
+        expect(page).to have_selector('img')
+        expect(page).to have_content('Allies')
+        expect(page).to have_content('Ty Lee')
+        expect(page).to have_content('Enemies')
+        expect(page).to have_content('Appa')
+        expect(page).to have_content('Affiliation')
+        expect(page).to have_content('Fire Nation circus')
       end
     end
   end
