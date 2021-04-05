b78811c389257fb93241b553d4de4ddbf62f2c38
Wil McCauley <wmccauley724@gmail.com>
2021-04-05 10:20:44 -0600

    Continue adding content to search result page

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -17,12 +17,18 @@ RSpec.describe 'the welcome index page' do
     click_button 'Search For Members'
 
     expect(current_path).to eq(search_path)
-
+    save_and_open_page
     within('#total') do
       expect(page).to have_content('Total People: 97')
     end
     within('.info') do
-      
+      expect(page).to have_selector('li', count: 25)
+      within('#5cf5679a915ecad153ab68fd') do
+        expect(page).to have_content('Name: Chan (Fire Nation admiral)')
+      end
+      within('#5cf5679a915ecad153ab6906') do
+
+      end
     end
   end
 end
